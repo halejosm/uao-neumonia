5f1cf2e94a48141e6461476c9c9ac8204ecedddf
Alejandro Sanchez <alesan@comfenalcoquindio.co>
2025-01-28 21:19:09 -0500

    reubicar archivos reportes

--- src/models/predictor.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-class Predictor:
-    def __init__(self):
-        self.image_array = None
-        self.label = ""
-        self.probability = 0.0
-        self.heatmap = None
